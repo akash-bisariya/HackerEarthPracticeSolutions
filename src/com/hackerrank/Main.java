049cfc44c826e9ca357f9a40c8f8a6794706181f
akashb <akashb@Rahulc-MacAir.local>
2019-08-20 12:37:21 +0530

    NeutralisationOfCharges,ChargedUpArray,PepperAndContiguousEvenSubArray

---
+++ src/com/hackerrank/Main.java
@@ -0,0 +1,17 @@
+package com.hackerrank;
+
+import com.hackerrank.practices.ChargedUpArray;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        ChargedUpArray.startSolution();
+//        NeutralisationOfCharges.startSolution();
+//        PepperAndContiguousEvenSubArray.startSolution();
+
+    }
+}
+
