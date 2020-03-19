6cff57180d4c17f70f2446c0b7e1e4dfcae4aa70
Amrit Sahani <situnamrit@gmail.com>
2020-03-19 13:54:31 +0530

    Update FindProduct.java
    As long data type is not present, the code shows error as data goes in cyclic integers

--- src/com/hackerrank/practices/basicInputOutput/FindProduct.java
+++ src/com/hackerrank/practices/basicInputOutput/FindProduct.java
@@ -1,33 +1,22 @@
-package com.hackerrank.practices.basicInputOutput;
-
-import com.hackerrank.practices.DataStructure.IPracticeSolution;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class FindProduct implements IPracticeSolution {
-
-    /**
-     * Link - https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/find-product/
-     * @throws IOException
-     */
-
-    @Override
-    public void startSolution() throws IOException {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        bufferedReader.readLine();
-        String[] input = bufferedReader.readLine().split(" ");
-
-        int product = 1;
-        int m = 1000000007;
-        for (int i=0;i<input.length;i++){
-//            product = ((product%m)*((Integer.parseInt(input[i]))%m))%m;
-            product = ((product%m)*Integer.parseInt(input[i]))%m;
-        }
-
-        System.out.println(product);
+class TestClass 
+{
+    public static void main(String args[] ) throws Exception 
+    {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int testcases = Integer.parseInt(br.readLine());
+      String x[] = br.readLine().split(" ");
+      long answer = 1;
+      long m = 1000000007;
+      for (int i=0;i < x.length;i++)
+      {
+          //product = ((product%m)*((Integer.parseInt(input[i]))%m))%m;
+            answer = ((answer)*Integer.parseInt(x[i]))%m;
+      }
+      System.out.print(answer);
 
     }
 }
